97cd3b91b1126088a474157a462d463f3cf1b237
KBryan <kwame@3einteractive.com>
2016-02-04 12:01:47 -0500

    Added Views to iOS application

--- MopalsApp/index.ios.js
+++ MopalsApp/index.ios.js
@@ -2,51 +2,74 @@
  * Sample React Native App
  * https://github.com/facebook/react-native
  */
-'use strict';
-import React, {
+'use strict'
+
+var React = require('react-native')
+var {
   AppRegistry,
-  Component,
   StyleSheet,
   Text,
-  View
-} from 'react-native';
+  View,
+  NavigatorIOS,
+} = React;
 
-class MopalsApp extends Component {
-  render() {
+var mainView = React.createClass({
+  render:function() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
-    );
-  }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+      <NavigatorIOS
+        barTintColor='#48BBEC'
+        titleTextColor ='#FFFFFF'
+        style={styles.navigator}
+        initialRoute={{
+          title: 'Mopals',
+          component: MopalsApp
+       }} />
+     
+     );
   },
 });
-
-AppRegistry.registerComponent('MopalsApp', () => MopalsApp);
+var Home = React.createClass({
+  render:function() {
+    return (
+      <View style={styles.container}>
+          <View style={styles.topBox} />
+          <View style={styles.bottomBox} />
+      </View>
+      );
+  }
+});
+var MopalsApp = React.createClass({
+  render:function() {
+    return (
+          <View style={styles.container}>
+            <Text>
+             Hello World !!!
+            </Text>
+            <View style={styles.topBox} />
+            <View style={styles.bottomBox} />
+          </View>
+      );
+    }
+});
+var styles = StyleSheet.create({
+    container: {
+      flex:1,
+      flexDirection:'column'
+    },
+    welcome: {
+      fontSize:25,
+      textAlign:'center'
+    },
+    navigator: {
+      flex:1
+    },
+    topBox: {
+      flex:2,
+      backgroundColor:'#CCE5FF'
+    },
+    bottomBox: {
+      flex:1,
+      backgroundColor:'#FFFFCC'
+    }
+});
+AppRegistry.registerComponent('MopalsApp', () => mainView);
